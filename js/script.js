e80c9fd1cc08730d6fc6b31ffe1000add03d6c71
ankit13130 <guptaankit13130@gmail.com>
2024-12-18 17:34:00 +0530

    Fixed age calculation

--- js/script.js
+++ js/script.js
@@ -22,13 +22,17 @@ for(i=0;i<navList.length;i++){
   })
 }
 
-//calculating age based on server date to avoid false local system date
-fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
-  .then(response => response.json())
-  .then(data => {
-      const serverDate = new Date(data.utc_datetime);
-      const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
-      document.getElementById('age').innerText = Math.floor((serverDate - new Date('2002/03/03')) / MS_IN_YEAR);
-  })
-  .catch(error => console.error('Error fetching NTP time:', error));
+//calculating age
+const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
+document.getElementById('age').innerText = Math.floor((new Date() - new Date('2002/03/03')) / MS_IN_YEAR);
+
+//calculating age based on server date to avoid false local system date (not working in netlify hosting)
+// fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
+//   .then(response => response.json())
+//   .then(data => {
+//       const serverDate = new Date(data.utc_datetime);
+//       const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
+//       document.getElementById('age').innerText = Math.floor((serverDate - new Date('2002/03/03')) / MS_IN_YEAR);
+//   })
+//   .catch(error => console.error('Error fetching NTP time:', error));
   
