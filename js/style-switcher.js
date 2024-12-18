0034625d0d042892bb5145190e145548dd350e67
ankit13130 <guptaankit13130@gmail.com>
2024-12-18 17:20:27 +0530

    Added dymanic feature for age calculation and fixed some styling, fix click event in theme change button

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,52 +2,58 @@
 const container = document.querySelector('.switch-toggler')
 const switchBtn = document.querySelector('.switch-toggler-style')
 
-var switchFlag = 0
+var toggleTheme = false
 
-switchBtn.addEventListener('click',()=>{
-    if(switchFlag === 0){
-        switchFlag = 1
-        container.style.transform = "translateX(0%)"
-    }
-    else{
-        switchFlag = 0
-        container.style.transform = "translateX(100%)"
-    }
-})
+switchBtn.addEventListener('click', () => {
+    toggleTheme = !toggleTheme;
+    container.style.transform = toggleTheme ? "translateX(0%)" : "translateX(100%)";
+});
 
 // hide style -switcher on scroll or click in window
-window.addEventListener('scroll',()=>{
-    switchFlag = 1
-    container.style.transform = "translateX(100%)"
+window.addEventListener('scroll', () => {
+    if (toggleTheme){
+        toggleTheme = !toggleTheme;
+        container.style.transform = "translateX(100%)";
+    }
 })
 
 // changing theme color 
 const alternateStyle = document.querySelectorAll('.alternate-style')
 
-function setActiveStyle (color){
+function setActiveStyle(color) {
     alternateStyle.forEach((style) => {
-        if(color === style.getAttribute("title")){
+        if (color === style.getAttribute("title")) {
             style.removeAttribute("disabled")
         }
-        else{
-            style.setAttribute("disabled","true")
+        else {
+            style.setAttribute("disabled", "true")
         }
     });
 }
 
 // theme change to light and dark
+// additionally by default it checks system theme, if system is at dark mode then it applies dark theme and vice versa.
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const dayNight = document.querySelector('.day-night')
 
-dayNight.addEventListener('click',()=>{
+if (prefersDarkMode) {
+    dayNight.querySelector('i').classList.add('ri-sun-fill')
+    document.body.classList.add('dark')
+} else {
+    dayNight.querySelector('i').classList.add('ri-moon-fill')
+    document.body.classList.remove('dark')
+}
+
+dayNight.addEventListener('click', () => {
     dayNight.querySelector('i').classList.toggle('ri-sun-fill')
     dayNight.querySelector('i').classList.toggle('ri-moon-fill')
     document.body.classList.toggle('dark')
 })
 
-window.addEventListener('load',()=>{
-    if(document.body.classList.contains('dark'))
+window.addEventListener('load', () => {
+    if (document.body.classList.contains('dark'))
         dayNight.querySelector('i').classList.add("ri-sun-fill")
-    else{
+    else {
         dayNight.querySelector('i').classList.add("ri-moon-fill")
     }
 })
